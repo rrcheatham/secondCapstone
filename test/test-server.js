b46def90c2a8ee52e2c754febcea1e4a20f03f0b
rrcheatham <rrcheatham@gmail.com>
2018-06-06 10:35:29 -0400

    set basic client wireframe

--- test/test-server.js
+++ test/test-server.js
@@ -16,4 +16,32 @@ describe('index page', function() {
     });
 });
 
+describe('sign-up page', function() {
+    it('should exist', function() {
+        return chai.request(app)
+            .get('/sign-up.html')
+            .then(function(res) {
+                expect(res).to.have.status(200);
+            });
+    });
+});
 
+describe('account page', function() {
+    it('should exist', function() {
+        return chai.request(app)
+            .get('/account.html')
+            .then(function(res) {
+                expect(res).to.have.status(200);
+            });
+    });
+});
+
+describe('rest page', function() {
+    it('should exist', function() {
+        return chai.request(app)
+            .get('/reset.html')
+            .then(function(res) {
+                expect(res).to.have.status(200);
+            });
+    });
+});
